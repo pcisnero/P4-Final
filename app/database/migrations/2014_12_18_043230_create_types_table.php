3c11a0bb082e1a55b8bf7b150df03044571ccb68
Sam Seaborn <pcisnerosfernandez@g.harvard.edu>
2014-12-17 22:56:26 -0600

    more migrations

--- app/database/migrations/2014_12_18_043230_create_types_table.php
+++ app/database/migrations/2014_12_18_043230_create_types_table.php
@@ -42,3 +42,71 @@ class CreateTypesTable extends Migration {
 	}
 
 }
+
+-- phpMyAdmin SQL Dump
+-- version 4.2.7.1
+-- http://www.phpmyadmin.net
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 25-11-2014 a las 19:44:45
+-- Versión del servidor: 5.6.20
+-- Versión de PHP: 5.5.15
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Base de datos: `taskmanager`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `types`
+--
+
+CREATE TABLE IF NOT EXISTS `types` (
+`id` int(10) unsigned NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `updated_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `description` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `icon_url` varchar(255) COLLATE utf8_unicode_ci NOT NULL
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=8 ;
+
+--
+-- Volcado de datos para la tabla `types`
+--
+
+INSERT INTO `types` (`id`, `created_at`, `updated_at`, `description`, `icon_url`) VALUES
+(1, '2014-11-23 06:00:00', '2014-11-23 06:00:00', 'Personal', 'images/personal_icon.png'),
+(2, '2014-11-23 06:00:00', '2014-11-24 22:41:50', 'Office', 'images/work_icon.png'),
+(7, '2014-11-24 08:56:34', '2014-11-24 09:08:29', 'Shopping', 'images/shop_icon.png');
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `types`
+--
+ALTER TABLE `types`
+ ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `types`
+--
+ALTER TABLE `types`
+MODIFY `id` int(10) unsigned NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=8;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
